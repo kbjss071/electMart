1a1364ff6e47bfabcbe103c163c3e1247c1808f8
Jin Kang <kbjss071@gmail.com>
2023-05-11 23:44:37 -0400

    accounts model admin

--- electMart/electMart/settings.py
+++ electMart/electMart/settings.py
@@ -74,6 +74,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'electMart.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
