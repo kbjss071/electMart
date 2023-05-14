93a52f3c9b48569cd6ee80948d6d3ffcc9a8e1e2
Jin Kang <kbjss071@gmail.com>
2023-05-13 20:21:56 -0400

    configure django media files and create custom password field

--- electMart/electMart/settings.py
+++ electMart/electMart/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
