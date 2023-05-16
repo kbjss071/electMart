b46a0dee7c51cc57a9142c9d59733cf66d63f1ea
Jin Kang <kbjss071@gmail.com>
2023-05-15 22:42:05 -0400

    implementing database to home.html and first attempt to make superuser accessible to admin

--- electMart/accounts/models.py
+++ electMart/accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password=None):
+    def create_user(self, first_name, last_name, username, email, password=None, **kwargs):
         if not email:
             raise ValueError('User must have an email address')
         
@@ -21,7 +21,7 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, first_name, last_name, email, username, password):
+    def create_superuser(self, first_name, last_name, email, username, password, **kwargs):
         user = self.create_user(
             email = self.normalize_email(email),
             username = username,
@@ -32,12 +32,15 @@ class MyAccountManager(BaseUserManager):
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
-        user.is_superadmin = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
+    
+    def get_by_natural_key(self, username):
+        return super().get_by_natural_key(username)
 
 
-class Account(AbstractBaseUser):
+class Account(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
@@ -49,15 +52,17 @@ class Account(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+    objects = MyAccountManager()
+
     def __str__(self):
         return self.email
     
-    def has_perm(self, permission, obj=None):
+    def has_perm(self, perm, obj=None):
         return self.is_admin
     
     def has_module_perm(self, add_label):
