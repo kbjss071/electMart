342d9471e7558aedb26ee683d1066a5517d33192
Jin Kang <kbjss071@gmail.com>
2023-05-25 11:22:31 -0400

    account register

---
+++ electMart/accounts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control'
+    }))
+
+    check_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm your password',
+        'class': 'form-control'
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['username', 'first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter Username'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.geT('password')
+        check_password= cleaned_data.get('check_password')
+
+        if password != check_password:
+            raise forms.ValidationError(
+                "password does not match"
+            )
